94dfecc95e6e36773d5708bf3cc234a0deab9507
campbell <78052886+campbe1104@users.noreply.github.com>
2023-01-16 10:48:36 +0000

    example file has been created

---
+++ MyString.java
@@ -0,0 +1,21 @@
+
+public class MyString {
+	
+	/*
+	 * length("Java")
+	 * -> (1 + length( "ava" ))
+	 * -> (1 + (1 + length( "va" )))
+	 * -> (1 + (1 + (1 + length( "a" ))))
+	 * -> (1 + (1 + (1 + (1 + length( "" )))))
+	 * -> (1 + (1 + (1 + (1 + 0))))
+	 * -> 4
+	 */
+	public static int length(String str) {
+		//TODO base case
+		
+		// TODO recursive case
+		
+		return -1; // dummy return value
+	}
+	
+}
